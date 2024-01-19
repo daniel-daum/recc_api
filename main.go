470f3f77a453d13105b4aa78c84c10d6206a620a
daniel daum <daniel_daum@outlook.com>
2024-01-18 22:43:01 -0800

    feat: added first dev routes

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", GetRoot)
+	mux.HandleFunc("/hello", GetHello)
+
+	fmt.Println("Starting server on port: 8000.")
+
+	error := http.ListenAndServe(":8000", mux)
+
+	if errors.Is(error, http.ErrServerClosed) {
+
+		fmt.Printf("Server closed.\n")
+
+	} else if error != nil {
+
+		fmt.Printf("Error starting server: %s\n", error)
+		os.Exit(1)
+
+	}
 }
