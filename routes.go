98978f2ca0007049ae3d7f8da085b339f2a13265
daniel daum <daniel_daum@outlook.com>
2024-05-13 19:59:33 -0700

    add scalar/swagger documentation

--- routes.go
+++ routes.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
+
+	"github.com/MarceloPetrucio/go-scalar-api-reference"
 )
 
 type HealthResponse struct {
-	Status string `json:"status"`
+	Status string `json:"status" example:"healthy"`
 }
 
+// HealthCheck godoc
+// @Summary      Health
+// @Description  Returns "healthy" API status
+// @Produce      json
+// @Success  	 200  object  HealthResponse  "HealthResponse JSON"
+// @Router       /health [get]
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 
 	healthResponse := HealthResponse{Status: "healthy"}
@@ -28,4 +37,20 @@ func HealthCheck(w http.ResponseWriter, r *http.Request) {
 			slog.String("agent", r.UserAgent()),
 		)
 	}
+}
+
+func Reference(w http.ResponseWriter, r *http.Request) {
+	htmlContent, err := scalar.ApiReferenceHTML(&scalar.Options{
+		SpecURL: "./docs/swagger.json",
+		CustomOptions: scalar.CustomOptions{
+			PageTitle: "Recc API",
+		},
+		DarkMode: true,
+	})
+
+	if err != nil {
+		slog.Error("error", err)
+	}
+
+	fmt.Fprintln(w, htmlContent)
 }
